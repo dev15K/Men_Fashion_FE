6ddae2d8cefc6d2cd27c7b344497306721ac1db9
dainqd <ngodaix5tp@gmail.com>
2024-11-15 13:59:26 +0700

    Admin chart.

--- src/Public/Components/Service/AdminService.js
+++ src/Public/Components/Service/AdminService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const API_ENDPOINT = {
     ADMIN_DASHBOARD: "/api/admin/dashboard",
+    ADMIN_CHART_ORDER: "/api/admin/chart-orders",
 }
 
 class AdminService {
@@ -16,6 +17,15 @@ class AdminService {
         return axios.get(BASE_URL_SERVER + API_ENDPOINT.ADMIN_DASHBOARD + '?type=' + type + '&size=' + size + '&sort=' + sort + '&keyword=' + keyword, config);
     }
 
+    adminChartOrder = (type) => {
+        const config = {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem("accessToken")}`
+            }
+        };
+        return axios.get(BASE_URL_SERVER + API_ENDPOINT.ADMIN_CHART_ORDER + '?type=' + type, config);
+    }
 }
 
 const adminService = new AdminService();
